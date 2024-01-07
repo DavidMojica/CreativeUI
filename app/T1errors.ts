20bdf73c7006cd6e88095bee73a3171d7aa046dc
David Mojica <davidmojicav@gmail.com>
2024-01-07 16:33:35 -0500

    Stable & Docstringed

--- app/T1errors.ts
+++ app/T1errors.ts
@@ -1,13 +1,48 @@
-export class ParameterDurationError extends Error {
-    constructor(element: HTMLElement | null, c:string="") {
-        super(`Invalid duration. On ${element.outerHTML} the class ${c} expects a number to determine the duration in seconds or 'i' to set it to infinite mode if the animation has infinite mode.`);
-        this.name = 'ParameterDurationError';
+// eslint-disable-next-line @typescript-eslint/no-namespace
+export namespace Errors{
+    /**
+     * Error thrown when an invalid duration is provided for an animation.
+     *
+     * @class
+     * @extends Error
+     * @namespace Errors
+     * @author D. Mojica
+     */
+    export class ParameterDurationError extends Error {
+        /**
+         * Creates a new instance of ParameterDurationError.
+         *
+         * @constructor
+         * @param {HTMLElement | null} element - The HTML element associated with the error.
+         * @param {string} c - The class name or identifier associated with the error.
+         * @author D. Mojica
+         */
+        constructor(element: HTMLElement | null, c:string="") {
+            super(`Invalid duration. On ${element.outerHTML} the class ${c} expects a number to determine the duration in seconds or 'i' to set it to infinite mode if the animation has infinite mode.`);
+            this.name = 'ParameterDurationError';
+        }
     }
-}
 
-export class AnimationHasNotInfiniteError extends Error {
-    constructor(element: HTMLElement | null, c:string="") {
-        super(`The animation ${c} does not support infinite mode. Element -> ${element.outerHTML}`);
-        this.name = 'ParameterDurationError';
+    /**
+     * Error thrown when an animation does not support infinite mode.
+     *
+     * @class
+     * @extends Error
+     * @namespace Errors
+     * @author D. Mojica
+     */
+    export class AnimationHasNotInfiniteError extends Error {
+        /**
+         * Creates a new instance of AnimationHasNotInfiniteError.
+         *
+         * @constructor
+         * @param {HTMLElement | null} element - The HTML element associated with the error.
+         * @param {string} c - The class name or identifier associated with the error.
+         * @author D. Mojica
+         */
+        constructor(element: HTMLElement | null, c:string="") {
+            super(`The animation ${c} does not support infinite mode. Element -> ${element.outerHTML}`);
+            this.name = 'ParameterDurationError';
+        }
     }
 }
--- app/T2actions.ts
+++ app/T2actions.ts
@@ -1,4 +1,4 @@
-import { AnimationHasNotInfiniteError, ParameterDurationError } from "./T1errors"
+import { Errors as E } from "./T1errors"
 
 // eslint-disable-next-line @typescript-eslint/no-namespace
 export namespace Actions {
@@ -6,19 +6,28 @@ export namespace Actions {
    * Abstract class to setup & deploy animations.
    * This class can not be implemented directly, the main objective is deploy it throuhg the childs classes.
    */
-  abstract class Animations {
-    /**
-     * This method renderizes the animation
-     * @param element
-     * @param duration
-     * @param steps
-     */
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    protected static Animate (element: HTMLElement, duration: number | string, steps?: number): void {}
-  }
-
-  // Typing machine effect
+  abstract class Animations { }
+  /**
+   * Represents a class for creating a typing machine effect on an HTML element.
+   *
+   * @class
+   * @extends Animations
+   * @namespace Actions
+   * @author D. Mojica
+   */
   export class Typing extends Animations {
+    /**
+     * Animates a typing effect on an HTML element.
+     *
+     * @param {HTMLElement} element - The HTML element on which the typing animation will be applied.
+     * @param {number | string} duration - The duration of the animation in seconds or 'i' for infinite.
+     * @param {number} steps - The number of steps in the animation.
+     * @returns {void}
+     * @protected
+     * @example
+     * // Example usage:
+     * this.Animate(myElement, 3, 10);
+     */
     protected static Animate (element: HTMLElement, duration: number | string, steps: number): void {
       console.log(`typing ${duration} steps(${steps}), blink 0.5s infinite step-end alternate`)
       element.style.animation = `typing ${duration} steps(${steps}), blink 0.5s infinite step-end alternate`
@@ -29,23 +38,27 @@ export namespace Actions {
     }
 
     /**
-     * desc ---
-     * @param {HTMLElement} element
-     * @param {number | string} duration
-     * @param {number} steps
+     * Starts typing machine animation.
+     * @param {HTMLElement} element - The HTML element on which the animation will be applied.
+     * @param {number | string} duration - The duration of the animation in seconds or 'i' for infinity.
+     * @param {number} steps - The number of steps in the animation. Corresponds to the length of the text.
+     * @throws {AnimationHasNotInfiniteError} - Fired if the duration is 'i', but the animation does not support infinite duration.
+     * @throws {ParameterDurationError} - Thrown if the duration is not a valid number or is a letter.
      * @returns {void}
      * @author MojiDIos
-     * @class
-     * @access Protected
-     * @example --
+     * @method
+     * @access public
+     * @example 
+     * //Example case:
+     * Animations.Typing.Play(myElement, 3, 10);
      */
     public static Play (element: HTMLElement, duration: number | string, steps: number): void {
       switch (true) {
-        case duration === 'i': throw new AnimationHasNotInfiniteError(element, 'creative-typing')
+        case duration === 'i': throw new E.AnimationHasNotInfiniteError(element, 'creative-typing')
         case !isNaN(Number(duration)):
           this.Animate(element, `${duration}s`, steps)
           break
-        default: throw new ParameterDurationError(element, 'creative-typing-')
+        default: throw new E.ParameterDurationError(element, 'creative-typing-')
       }
     }
   }
